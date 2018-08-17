f523fff439273c491844ae55dc87559ddbeca19c
Willy <willychai05@gmail.com>
2018-08-17 17:08:36 +0700

    fix menu problem

--- header.php
+++ header.php
@@ -1,8 +1,12 @@
 <nav class="w3-sidenav w3-animate-left w3-cyan w3-card-12" style="display: none; width: 20%;" id="sidenav">
 <a class="w3-hover-white" style="color: white; font-size: 19px; padding-top: 10px; cursor: pointer;" onclick="closeNav()"><b>Close X</b></a>
 <a href="index.php" class="w3-hover-white" style="color: white;"><i class="fas fa-home" style="margin-right: 20px;"></i>Home</a>
+<a class="w3-hover-white" href="keranjang.php"><i class="fas fa-chart-pie" style="margin-right: 20px;"></i>Keranjang</a>
+<a class="w3-hover-white" href="confirmation.php"><i class="fas fa-chart-pie" style="margin-right: 20px;"></i>Konfirmasi Pesanan</a>
+<a class="w3-hover-white" href="kontak.php"><i class="fas fa-chart-pie" style="margin-right: 20px;"></i>Pengaturan Kontak</a>
+<a href="#" onclick="signOut();"><i class="fas fa-chart-pie" style="margin-right: 20px;"></i>Log Out</a>
 	<div class="w3-dropdown-hover">
-      <a class="w3-hover-white" id="kategori"><i class="fas fa-chart-pie" style="margin-right: 20px;"></i>Kategori</a>
+      	<a class="w3-hover-white" id="kategori"><i class="fas fa-chart-pie" style="margin-right: 20px;"></i>Kategori</a>
       <div class="w3-dropdown-content w3-card-4" style="width: 200px; transition: 0.5s;">
       	<?php
 			$sql = "SELECT * FROM category";
@@ -18,9 +22,29 @@
       </div>
     </div>
 </nav>
+<div class="header w3-cyan">
+	<button class="menuButton w3-hover-white" onclick="openNav()"><i class="fas fa-bars fa-2x"></i></button>
+	<p class="toko"><i>Toko Kamanda Shop</i></p>
+	<input type="text" name="search" placeholder="Cari produk" class="search">
+
+	<div id="googleSignIn" class="g-signin2" data-onsuccess="onSignIn" data-theme="dark" style="float: right;"></div> 
+    <script> 
+      function onSignIn(googleUser) { 
+        // Useful data for your client-side scripts: 
+        var profile = googleUser.getBasicProfile(); 
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server! 
+        console.log('Full Name: ' + profile.getName()); 
+        console.log('Given Name: ' + profile.getGivenName()); 
+        console.log('Family Name: ' + profile.getFamilyName()); 
+        console.log("Image URL: " + profile.getImageUrl()); 
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend: 
+        var id_token = googleUser.getAuthResponse().id_token; 
+        console.log("ID Token: " + id_token); 
+      }; 
+	</script>
+	<img src="" alt="Profil" id="profileGoogle" style="display: none">
+</div>
 <div class="menuProfil w3-card-4 out" id="menuProfil" onmouseover="menuProfilIn()" onmouseout="menuProfilOut()">
-	<a href="keranjang.php"><button>Keranjang</button></a><br>
-	<a href="confirmation.php"><button>Konfirmasi Pesanan</button></a><br>
-	<a href="user/kontak.php"><button>Pengaturan Kontak</button></a><br>
-	<a href="#" onclick="signOut();"><button>Log Out</button></a>
 </div>
--- index.php
+++ index.php
@@ -1,76 +1,17 @@
 <?php
 	include "config/config.php";
-	session_start();
-
-	if (isset($_SESSION['status']) && isset($_SESSION['idToken'])) {
-		# code...
-	}
+	include "config/sessionUser.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Toko Kamanda Shop</title>
-	<meta name="google-signin-scope" content="profile email"> 
-    <meta name="google-signin-client_id" content="571963356124-9nhkogpvo06cmqjnav3qh8cv3848n6na.apps.googleusercontent.com"> 
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <link rel="stylesheet" type="text/css" href="style/w3.css">
-	<link rel="stylesheet" type="text/css" href="style/css.css">
-	<link rel="stylesheet" type="text/css" href="style/all.css">
-	<script src="js/ajax.js"></script>
-	<script src="js/login.js"></script>
-	<script src="js/all.js"></script>
-	<script>
-		function openNav(){
-			document.getElementById('sidenav').style.display="block";
-		}
-		function closeNav(){
-			document.getElementById('sidenav').style.display="none";
-		}
-		function kategoriColorWhite(){
-			document.getElementById('kategori').classList.add('w3-white');
-		}
-		function kategoriColorCyan(){
-			document.getElementById('kategori').classList.remove('w3-white');
-		}
-		function menuProfilIn(){
-            document.getElementById('menuProfil').classList.add('in');
-            document.getElementById('menuProfil').classList.remove('out');
-        }
-        function menuProfilOut(){
-            document.getElementById('menuProfil').classList.remove('in');
-            document.getElementById('menuProfil').classList.add('out');
-        }
-	</script>
+	<?php include "head.php"; ?>
 </head>
 <body>
+
 <?php include "header.php"; ?>
 
-<div class="header w3-cyan">
-	<button class="menuButton w3-hover-white" onclick="openNav()"><i class="fas fa-bars fa-2x"></i></button>
-	<p class="toko"><i>Toko Kamanda Shop</i></p>
-	<input type="text" name="search" placeholder="Cari produk" class="search">
-
-	<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark" style="float: right;"></div> 
-    <script> 
-      function onSignIn(googleUser) { 
-        // Useful data for your client-side scripts: 
-        var profile = googleUser.getBasicProfile(); 
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server! 
-        console.log('Full Name: ' + profile.getName()); 
-        console.log('Given Name: ' + profile.getGivenName()); 
-        console.log('Family Name: ' + profile.getFamilyName()); 
-        console.log("Image URL: " + profile.getImageUrl()); 
-        console.log("Email: " + profile.getEmail());
-
-        // The ID token you need to pass to your backend: 
-        var id_token = googleUser.getAuthResponse().id_token; 
-        console.log("ID Token: " + id_token); 
-      }; 
-	</script>
-	
-</div>
-
-<div class="isi">
+<div style="margin: 30px -20px 20px 80px;">
 <?php
 	$sql = "SELECT product.idProduct, 
 			product.sellingPrice, 
